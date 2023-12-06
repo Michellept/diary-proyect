607cf5eca6724dddb8f355377c74a836163daf59
Michellept <thocokun@hotmail.com>
2023-12-06 16:01:38 -0600

    falta enpoint getOne?

--- diary-proyect/src/app/module/pages/contact/pages/details-contact/details-contact/details-contact.component.ts
+++ diary-proyect/src/app/module/pages/contact/pages/details-contact/details-contact/details-contact.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { ContactService } from 'src/app/module/services/contact.service';
 
 @Component({
   selector: 'app-details-contact',
   templateUrl: './details-contact.component.html',
-  styleUrls: ['./details-contact.component.scss']
+  styleUrls: ['./details-contact.component.scss'],
 })
-export class DetailsContactComponent {
+export class DetailsContactComponent implements OnInit {
+  @Input() idContact?: number;
 
+  private _contactService = inject(ContactService);
+  constructor() {}
+
+  ngOnInit(): void {
+    // if (this.idContact) {
+    //   this._contactService.getAllContacts(this.idContact).subscribe({
+    //     next: (response) => {
+    //       console.log(response);
+    //     },
+    //   });
+    // }
+  }
 }
