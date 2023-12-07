501251fb17a266a978236f6ceb2192f62edc0a24
Michelle Patiño Campos <thocokun@gmail.com>
2023-12-06 22:18:57 -0600

    create details view

--- diary-proyect/src/app/module/pages/contact/pages/details-contact/details-contact/details-contact.component.ts
+++ diary-proyect/src/app/module/pages/contact/pages/details-contact/details-contact/details-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContactService } from 'src/app/module/services/contact.service';
 
 @Component({
@@ -9,8 +10,36 @@ import { ContactService } from 'src/app/module/services/contact.service';
 export class DetailsContactComponent implements OnInit {
   @Input() idContact?: number;
 
+  public formEditContact!: FormGroup;
   private _contactService = inject(ContactService);
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.formEditContact = this.fb.group({
+      contactName: ['', Validators.required],
+      contactLastName: ['', Validators.required],
+      contactPhone: [''],
+      contactEmail: [''],
+      contactNotes: [''],
+      contactPhoto: [''],
+      contactAlias: [''],
+      contactBirthday: ['', Validators.required],
+      contactCompany: [''],
+    });
+  }
+
+  dataTable = [
+    {
+      contactId: 1,
+      contactName: 'Jhon',
+      contactLastName: 'Doe',
+      contactEmail: '5kS6e@example.com',
+      contactAlias: 'Jhon Doe',
+      contactCompany: 'Company 1',
+      contactBirthday: '1990-01-01',
+      contactPhoto: 'https://via.placeholder.com/150',
+      contactNotes: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      contactPhone: '123456789',
+    },
+  ];
 
   ngOnInit(): void {
     // if (this.idContact) {
@@ -20,5 +49,10 @@ export class DetailsContactComponent implements OnInit {
     //     },
     //   });
     // }
+  }
+
+  isValidField(field: string): boolean | null {
+    const control = this.formEditContact.controls[field];
+    return control && control.invalid && control.touched;
   }
 }
